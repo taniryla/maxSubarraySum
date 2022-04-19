ecd444a2df23e37c60f49d7aae50b68e1b9b9b20
Ryland Taniguchi <ryland0000@gmail.com>
2022-04-19 01:21:58 +0100

    poyla step 1

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@
 
 // Step One (Understand). We have to understand the problem.
 // 1. What is the unknown?
+the function should calculate the maximum sum of n consecutive elements in the array
+
+
 // 2. What are the data inputs?
 array of integers and a number called n
 
 // 3. Can you restate the problem in your own words?
+
+n is the number of consecutive spots; find the max value of n spots
+
 // 4. Can the unknown be determined from the data inputs?
+yes 
+
 // 5. Is the data inputs sufficient to determine the unknown? Insufficient? Redundant? Contradictory? 
+yes 
+
 // 6. How should I label important pieces of data input that are a part of the problem?
 arr, n
 
@@ -16,6 +26,7 @@ arr, n
 
 // Step Two (Plan). Come up with concrete examples to help you understand the problem better. Find a connection between the data inputs and the unknown. You may be obliged to consider auxiliary problems if an immediate connection cannot be found. 
 // 9.  Have you seen it before? Or have you seen the same problem in a slightly different form? Do you know a related problem? 
+
 // 10.   Do you know a coding strategy that could be useful? Look at the unknown! And try to think of a familiar problem having the same or a similar unknown. Here is a problem related to yours and solve before. Could you use it? Could you use its result? Could you use its method? 
 // 11.    Should you introduce some auxiliary element in order to make its use possible? 
 // 12. Start with a simple example. Could you restate the problem? Could you restate it still differently? 
